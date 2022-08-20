dd03923343ee19969e19f333101613a06a4b2edc
caroline yougo <caroline.yougo@gmail.com>
2022-08-20 19:52:53 -0400

    conflict 2 resolved

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 deploy java application
 Deploy python apps
-deploy in the dev env
+deploy in the dev envD
 deployment5 master
 deployment4
+deploy to staging env
+echo good job.
+tested and accepted
